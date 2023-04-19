1afaef1f0da6bb808de329d9d6d62d5a9dc57efc
GR <gr@fastball.dev>
2023-04-19 18:22:41 +0800

    fix: compile life cycle

--- fastball-compile/src/main/java/dev/fastball/compile/processor/FastballComponentCompileProcessor.java
+++ fastball-compile/src/main/java/dev/fastball/compile/processor/FastballComponentCompileProcessor.java
@@ -3,6 +3,7 @@ package dev.fastball.compile.processor;
 import dev.fastball.compile.CompileContext;
 import dev.fastball.compile.FastballCompileGenerator;
 import dev.fastball.compile.FastballPreCompileGenerator;
+import dev.fastball.core.annotation.UIComponent;
 import dev.fastball.core.material.MaterialRegistry;
 
 import javax.annotation.processing.AbstractProcessor;
@@ -10,11 +11,10 @@ import javax.annotation.processing.RoundEnvironment;
 import javax.annotation.processing.SupportedAnnotationTypes;
 import javax.annotation.processing.SupportedSourceVersion;
 import javax.lang.model.SourceVersion;
+import javax.lang.model.element.AnnotationMirror;
+import javax.lang.model.element.Element;
 import javax.lang.model.element.TypeElement;
-import java.util.List;
-import java.util.Objects;
-import java.util.ServiceLoader;
-import java.util.Set;
+import java.util.*;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 import java.util.stream.StreamSupport;
@@ -23,7 +23,6 @@ import java.util.stream.StreamSupport;
  * @author gr@fastball.dev
  * @since 2022/12/30
  */
-@SupportedAnnotationTypes("dev.fastball.core.annotation.UIComponent")
 @SupportedSourceVersion(SourceVersion.RELEASE_8)
 public class FastballComponentCompileProcessor extends AbstractProcessor {
 
@@ -31,15 +30,29 @@ public class FastballComponentCompileProcessor extends AbstractProcessor {
 
     private boolean preCompileDone = false;
 
+    private final Set<Element> firstElement = new HashSet<>();
+
     @Override
     public boolean process(Set<? extends TypeElement> annotations, RoundEnvironment roundEnv) {
-        if (preCompileDone) {
-            processCompile(roundEnv);
-        } else {
+        if (!preCompileDone) {
             processPreCompile(roundEnv);
             preCompileDone = true;
+            firstElement.addAll(roundEnv.getRootElements());
+            return false;
+        } else {
+            processCompile(roundEnv);
         }
-        return false;
+        return true;
+    }
+
+    @Override
+    public Set<String> getSupportedAnnotationTypes() {
+        Set<String> supportedAnnotationTypes = loadGeneratorStream(FastballPreCompileGenerator.class)
+                .flatMap(generator -> generator.getSupportedAnnotationTypes().stream())
+                .collect(Collectors.toSet());
+        loadGeneratorStream(FastballCompileGenerator.class).flatMap(generator -> generator.getSupportedAnnotationTypes().stream())
+                .forEach(supportedAnnotationTypes::add);
+        return supportedAnnotationTypes;
     }
 
     private void processPreCompile(RoundEnvironment roundEnv) {
@@ -55,6 +68,14 @@ public class FastballComponentCompileProcessor extends AbstractProcessor {
                         .map(element -> new CompileContext(processingEnv, roundEnv, materialRegistry, element))
                         .forEach(generator::generate)
         );
+        if (!firstElement.isEmpty()) {
+            loadGeneratorStream(FastballCompileGenerator.class).forEach(generator ->
+                    loadElements(firstElement, generator.getSupportedAnnotationTypes()).stream()
+                            .map(element -> new CompileContext(processingEnv, roundEnv, materialRegistry, element))
+                            .forEach(generator::generate)
+            );
+            firstElement.clear();
+        }
     }
 
     private <T> Stream<T> loadGeneratorStream(Class<T> clazz) {
@@ -63,5 +84,11 @@ public class FastballComponentCompileProcessor extends AbstractProcessor {
 
     private List<TypeElement> loadElements(RoundEnvironment roundEnv, Set<String> supportedAnnotationTypes) {
         return supportedAnnotationTypes.stream().map(annotationName -> processingEnv.getElementUtils().getTypeElement(annotationName)).filter(Objects::nonNull).flatMap(annotationType -> roundEnv.getElementsAnnotatedWith(annotationType).stream()).map(TypeElement.class::cast).collect(Collectors.toList());
+    }
+
+    private List<TypeElement> loadElements(Set<? extends Element> elements, Set<String> supportedAnnotationTypes) {
+        return supportedAnnotationTypes.stream().map(annotationName -> processingEnv.getElementUtils().getTypeElement(annotationName)).filter(Objects::nonNull).flatMap(annotationType -> elements.stream().filter(element ->
+                processingEnv.getElementUtils().getAllAnnotationMirrors(element).stream().anyMatch(a -> annotationType.equals(a.getAnnotationType().asElement()))
+        )).map(TypeElement.class::cast).collect(Collectors.toList());
     }
 }
