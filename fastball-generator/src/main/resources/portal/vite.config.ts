87f949bb13f62bd84a6aee278aec59dff664dfdc
GR <gr@fastball.dev>
2023-05-01 10:13:08 +0800

    fix: temp processing ant design pro 2.4.6 bug

--- fastball-generator/src/main/resources/portal/vite.config.ts
+++ fastball-generator/src/main/resources/portal/vite.config.ts
@@ -4,6 +4,9 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  define: {
+    'process.env': '{}',
+  },
   server: {
     proxy: {
       '/api': 'http://localhost:8080'
